e9aad3426e75e9528b918086926d255cb636818d
bernineu <wi23b089@technikum-wien.at>
2025-05-01 12:56:16 +0200

    adopt renderProfileSidebar

--- app/frontend/pages/user/profile.js
+++ app/frontend/pages/user/profile.js
@@ -1,2 +1,14 @@
 import {BACKEND_BASE_URL} from "../../util/rest.js";
 import { sanitizeInput } from "../../util/helper.js";
+import { checkLoginStatus } from "../../util/helper.js";
+import { renderUserSidebar } from "../../ui/sidebar.js";
+
+
+export async function renderProfilePage() {
+    const content = document.getElementById("content");
+    content.innerHTML = `
+        <h2>Profile Settings</h2>
+        <p>Here you can manage your personal information.</p>
+        <!-- Weitere Inhalte folgen -->
+    `;
+}
